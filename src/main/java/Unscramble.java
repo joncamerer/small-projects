a89c3635ba60d6b636c7df05cc1cdbf126be360a
Jon Camerer <jon.camerer@gmail.com>
2021-08-23 18:34:24 -0500

    fixed bug in wordTimes logic

--- src/main/java/Unscramble.java
+++ src/main/java/Unscramble.java
@@ -10,6 +10,35 @@ public class Unscramble {
         this.targets = targets;
     }
 
+    public String getLeftovers() {
+        Map<String, Integer> occurrences = getTargetOccurrences();
+        String fullString = "";
+
+        for (int i = 0; i < this.targets.length; i++) {
+            int times = occurrences.get(this.targets[i]);
+
+            for(int k = 0; k < times; k++) {
+                fullString += this.targets[i];
+            }
+        }
+
+        char[] wordArr = this.scrambledWord.toCharArray();
+
+        for (char letter : wordArr) {
+            if (fullString.indexOf(letter) != -1) {
+                int index = fullString.indexOf(letter);
+
+                if (index == 0) {
+                    fullString = fullString.substring(1);
+                } else {
+                    fullString = fullString.substring(0, index) + fullString.substring(index + 1);
+                }
+            }
+        }
+
+        return fullString;
+    }
+
     public Map<String, Integer> getTargetOccurrences() {
         Map <String , Integer> results = new HashMap<>();
 
@@ -26,7 +55,7 @@ public class Unscramble {
         //for each c in target
         for (Character c : target.toCharArray()) {
             //if c does not appear in word
-            if (word.indexOf(c) == -1) {
+            if (word.indexOf(c) == -1 || charTimes(c, target) > charTimes(c, word)) {
                 return 0;
             } else {
                 //map how many times c appears in word
@@ -41,12 +70,14 @@ public class Unscramble {
             }
         }
 
+        System.out.println(matches.toString());
+
         //return smallest value in matches
         int foundTimes = -1;
 
         for (Map.Entry<Character, Integer> entry : matches.entrySet()) {
             int timesInTarget = charTimes(entry.getKey(), target);
-            int adjustedTimes = (timesInTarget > 1) ? entry.getKey() / timesInTarget :entry.getValue();
+            int adjustedTimes = (timesInTarget > 1) ? entry.getKey() / timesInTarget : entry.getValue();
 
             if (foundTimes < 0) {
                 foundTimes = adjustedTimes;
@@ -71,13 +102,20 @@ public class Unscramble {
     }
 
     public static void main(String[] args) {
-        String word = "onefournine";
-        String word2 = "onethreefoursevennine";
+        String word = "inethre";
+        String word2 = "onefoursevennine";
         String[] targets = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
         Unscramble unscramble = new Unscramble(word, targets);
         Map<String, Integer> results = unscramble.getTargetOccurrences();
 
+        System.out.println();
+        System.out.println(word2);
+        System.out.println();
+
+        //Numberify Map
+        System.out.println("-----------------------");
+        System.out.print("As an int: ");
         for (int i = 0; i < targets.length; i++) {
             int times = results.get(targets[i]);
 
