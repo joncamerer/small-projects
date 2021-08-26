9a2930d8c8eedfa4d567fd12a0f36ed8fa8626b8
Jon Camerer <jon.camerer@gmail.com>
2021-08-26 16:20:39 -0500

    Added tests for uniqueOccurrencesToString

--- src/test/java/UnscrambleTest.java
+++ src/test/java/UnscrambleTest.java
@@ -36,4 +36,36 @@ public class UnscrambleTest {
 
         assertTrue(unscramble.chaseSolution().contains(expected));
     }
+
+    @org.junit.Test
+    public void uniqueOccurrencesToStringFor149() {
+        String scramble = "onenruofine";
+        String[] targets = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String expectedOne = "one: 1";
+        String expectedFour = "four: 1";
+        String expectedNine = "nine: 1";
+
+        Unscramble unscramble = new Unscramble(scramble, targets);
+        String actual = unscramble.uniqueOccurrencesToString();
+
+        assertTrue(actual.contains(expectedOne));
+        assertTrue(actual.contains(expectedFour));
+        assertTrue(actual.contains(expectedNine));
+    }
+
+    @org.junit.Test
+    public void uniqueOccurrencesToStringForAnyTargets() {
+        String scramble = "testwordwordexampleexampleexample";
+        String[] targets = {"test", "word", "example"};
+        String expectedTest = "test: 1";
+        String expectedWord = "word: 2";
+        String expectedExample = "example: 3";
+
+        Unscramble unscramble = new Unscramble(scramble, targets);
+        String actual = unscramble.uniqueOccurrencesToString();
+
+        assertTrue(actual.contains(expectedTest));
+        assertTrue(actual.contains(expectedWord));
+        assertTrue(actual.contains(expectedExample));
+    }
 }
